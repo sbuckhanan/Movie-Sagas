eac87dd9b59184a3601f210f1141b9738e9cb9f9
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-21 15:46:26 -0500

    add movelistitem component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,26 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import MovieListItem from '../MovieListItem/MovieListItem';
+import './MovieList.css';
 
 function MovieList() {
+	const dispatch = useDispatch();
+	const movies = useSelector((store) => store.movies);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+	useEffect(() => {
+		dispatch({ type: 'FETCH_MOVIES' });
+	}, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+	return (
+		<main>
+			<h1>MovieList</h1>
+			<section className='movies'>
+				{movies.map((movie) => {
+					return <MovieListItem movie={movie} key={movie.id} />;
+				})}
+			</section>
+		</main>
+	);
 }
 
 export default MovieList;
---
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+//? dispatch to sagas which movie I clicked on to do the get request.
+//? Put to reducer that information
+//? Update redux with what data I got from the database
+
+function MovieListItem({ movie }) {
+	const history = useHistory();
+	const dispatch = useDispatch();
+
+	const handleDetails = () => {
+		//? Dispatch here
+		dispatch({ type: 'GET_DETAILS', payload: movie.id });
+		history.push('/details');
+	};
+	return (
+		<div key={movie.id} onClick={handleDetails}>
+			<h3>{movie.title}</h3>
+			<img src={movie.poster} alt={movie.title} />
+		</div>
+	);
+}
+
+export default MovieListItem;
