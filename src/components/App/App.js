15958a3932a80618e5fd98c1a785c15c28e7c187
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-21 16:22:31 -0500

    finish MovieDetails component. Add to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,26 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+import MovieList from '../MovieList/MovieList';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
+	return (
+		<div className='App'>
+			<h1>The Movies Saga!</h1>
+			<Router>
+				<Route path='/' exact>
+					<MovieList />
+				</Route>
+				<Route path='/details' exact>
+					<MovieDetails />
+				</Route>
 
-        {/* Add Movie page */}
-      </Router>
-    </div>
-  );
+				{/* Details page */}
+
+				{/* Add Movie page */}
+			</Router>
+		</div>
+	);
 }
-
 
 export default App;
