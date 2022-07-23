a1ed4e42859b57ac2d12ce2c6cf7e777b7b5dfc5
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-22 21:10:12 -0500

    remove not used styling

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,10 +2,18 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MovieListItem from '../MovieListItem/MovieListItem';
 import './MovieList.css';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
 
 function MovieList() {
 	const dispatch = useDispatch();
 	const movies = useSelector((store) => store.movies);
+	const [searchString, setSearchString] = useState('');
 
 	//? get our movies on page load or refresh
 	useEffect(() => {
@@ -14,6 +22,26 @@ function MovieList() {
 
 	return (
 		<main>
+			<Box
+				className='form'
+				component='form'
+				sx={{
+					'& > :not(style)': { m: 1, width: '25ch' },
+				}}
+				noValidate
+				autoComplete='off'>
+				<FormControl fullWidth sx={{ m: 1, width: '100%' }}>
+					<InputLabel htmlFor='outlined-adornment'>Search</InputLabel>
+					<OutlinedInput
+						fullWidth
+						id='outlined-adornment'
+						value={searchString}
+						onChange={(e) => setSearchString(e.target.value)}
+						label='Search'
+					/>
+				</FormControl>
+				<Button>Save</Button>
+			</Box>
 			<h1>MovieList</h1>
 			<section className='movies'>
 				{movies.map((movie) => {
