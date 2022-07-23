5bd3e472b15b73fca300cbb2af8dd6803d3d593a
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-22 20:28:17 -0500

    remove index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App/App.js';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
