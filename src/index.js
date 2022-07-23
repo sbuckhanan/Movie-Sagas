22573b3dcf564f91cac49c431e6b7d48daf5f630
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-22 20:10:29 -0500

    move all reducers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './components/App/App.js';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
-// Import saga middleware
 import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+//? Reducers imports
+import movies from './redux/reducers/movies-reducer';
+import genres from './redux/reducers/genres-reducer';
+import details from './redux/reducers/details-reducer';
 
 // Create the rootSaga generator function
 function* rootSaga() {
@@ -55,43 +61,19 @@ function* updateMovie(action) {
 	}
 }
 
-// Used to store movies returned from the server
-const movies = (state = [], action) => {
-	switch (action.type) {
-		case 'SET_MOVIES':
-			return action.payload;
-		default:
-			return state;
-	}
-};
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
 
-// Used to store the movie genres
-const genres = (state = [], action) => {
-	switch (action.type) {
-		case 'SET_GENRES':
-			return action.payload;
-		default:
-			return state;
-	}
-};
-
-// Used to store the movie details that were clicked
-// Using an object because I am only working with 1 movie at a time
-// Also using these details on the edit page for the 1 movie I am working with
-const details = (state = { title: '', description: '', poster: '', array_agg: [] }, action) => {
-	switch (action.type) {
-		case 'SET_DETAILS':
-			return {
-				// update each of the properties with their new value that was sent over
-				title: action.payload.title,
-				description: action.payload.description,
-				poster: action.payload.poster,
-				array_agg: action.payload.array_agg,
-			};
-		default:
-			return state;
-	}
-};
+// Create one store that all components can use
+const storeInstance = createStore(
+	combineReducers({
+		movies,
+		genres,
+		details,
+	}),
+	// Add sagaMiddleware to our store
+	applyMiddleware(sagaMiddleware, logger)
+);
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
